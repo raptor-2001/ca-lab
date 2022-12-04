677c8c69490e85ceb4c1cb63a1931a3d7fc3da19
raptor-2001 <pratham007yadav@gmail.com>
2022-12-04 18:46:12 +0530

    re-creating the CA submission

---
+++ commands.sh
@@ -0,0 +1,31 @@
+sudo apt-get update
+sudo apt update
+sudo apt-get install build-essential gdb g++ gcc -y
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+pip install opencv-python
+sudo apt install mpich -y
+sudo apt-get install libopenmpi-dev -y
+sudo apt-get install openmpi-bin -y
+pip3 install mpi4py
+pip3 install scikit-image
+pip3 install pandas
+sudo apt-get install python3-sklearn -y
+pip3 install matplotlib
+pip3 install numpy
+sudo apt install binutils -y
+pip install -U scikit-learn scipy matplotlib
+
+
+sudo apt-get install vsftpd -y
+sudo systemctl start vsftpd
+sudo systemctl enable vsftpd
+sudo apt-get install git -y
+
+cd Pictures
+mkdir .iit_vishrambagh
+cd .iit_vishrambagh
+
+
+git clone https://github.com/raptor-2001/ca-lab
+
